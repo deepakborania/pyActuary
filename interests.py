5d6a936c6bf4de32d9dfc93c49955d30091a1898
Deepak Borania <deepakborania@users.noreply.github.com>
2014-11-12 00:54:59 +0530

    Interest Rate Conversions

--- interests.py
+++ interests.py
@@ -1,93 +1,97 @@
 from math import log, log1p, exp
 
 
-def simpleIntrFI(p, iRate, period):
-    return p * iRate * period
+def simple_intr_fi(p, irate, period):
+    return p * irate * period
 
 
-def simpleIntrFV(p, iRate, period):
-    return p + simpleIntrFI(p, iRate, period)
+def simple_intr_fv(p, irate, period):
+    return p + simple_intr_fi(p, irate, period)
 
 
-def cmpdIntrFI(a, iRate, period):
-    return cmpdIntrFV(a, iRate, period) - a
+def cmpd_intr_fi(a, irate, period):
+    return cmpd_intr_fv(a, irate, period) - a
 
 
-def cmpdIntrFV(a, iRate, period):
-    return a * (iRate + 1) ** period
+def cmpd_intr_fv(a, irate, period):
+    return a * (irate + 1) ** period
 
 
-def pvFactor(iRate, period=1):
-    return (1 + iRate) ** (-period)
+def pv_factor(irate, period=1):
+    return (1 + irate) ** (-period)
 
 
-def pv(fv, iRate, period):
-    return fv * pvFactor(iRate, period)
+def pv(fv, irate, period):
+    return fv * pv_factor(irate, period)
 
 
-def pvSimple(futureVal, discountRate, period):
-    return futureVal * (1 - period * discountRate)
+def pv_simple(futureval, discountrate, period):
+    return futureval * (1 - period * discountrate)
 
 
-def convertEffectiveRate(iRate, srcPrdLength, targetPrdLength):
-    return ((1 + iRate) ** (float(targetPrdLength) / srcPrdLength)) - 1
+def convert_effective_rate(irate, src_prd_length, target_prd_length):
+    return ((1 + irate) ** (float(target_prd_length) / src_prd_length)) - 1
 
 
-def iRateNominalToEffective(ip, p):
+def irate_nominal_to_effective(ip, p):
     return ((1 + (float(ip) / p)) ** p) - 1
 
 
-def irateEffectiveToNominal(i, p):
+def irate_effective_to_nominal(i, p):
     return p * (((1 + i) ** (1.0 / p)) - 1)
 
 
-def dRateNominalToEffective(dp, p):
+def drate_nominal_to_effective(dp, p):
     return 1 - ((1 - (float(dp) / p)) ** p)
 
 
-def dRateEffectiveToNominal(d, p):
+def drate_effective_to_nominal(d, p):
     return p * (1 - ((1 - d) ** (1.0 / p )))
 
 
-def iRateToForce(i):
+def irate_to_force(i):
     return log1p(i)
 
 
-def iRateToV(i):
-    return pvFactor(i)
+def irate_to_v(i):
+    return pv_factor(i)
 
 
-def iRateToDiscRate(i):
-    return i * pvFactor(i)
+def irate_to_discrate(i):
+    return i * pv_factor(i)
 
 
-def forceToiRate(delta):
+def force_to_irate(delta):
     return exp(delta) - 1
 
 
-def forceToV(delta):
+def force_to_v(delta):
     return exp(-delta)
 
 
-def forceToDiscRate(delta):
+def force_to_discrate(delta):
     return 1 - exp(-delta)
 
 
-def vToForce(v):
+def v_to_force(v):
     return -log(v)
 
 
-def vToiRate(v):
+def v_to_irate(v):
     return (float(v) ** (-1)) - 1
 
 
-def vToDiscRate(v):
+def v_to_discrate(v):
     return 1 - v
 
 
-def discRateToForce(d):
+def discrate_to_force(d):
     return -log(1 - d)
 
 
-def discRateToV(d):
+def discrate_to_irate(d):
+    return ((1 - d) ** (-1)) - 1
+
+
+def discrate_to_v(d):
     return 1 - d
